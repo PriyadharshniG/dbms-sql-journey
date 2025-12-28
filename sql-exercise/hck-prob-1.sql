eb4dfe647ffa48f02a52022aeb0739a85c02c017
Priyadharshni.G <155886762+PriyadharshniG@users.noreply.github.com>
2025-12-28 23:22:11 +0530

    sum-2

--- sql-exercise/hck-prob-1.sql
+++ sql-exercise/hck-prob-1.sql
@@ -1,2 +1,4 @@
---basic
-SELECT * FROM City where Population>100000 AND CountryCode='USA'; 
+--basic-select-query
+SELECT * FROM City where Population>100000 AND CountryCode='USA';
+
+SELECT Name from City WHERE Population>120000 AND CountryCode='USA';
