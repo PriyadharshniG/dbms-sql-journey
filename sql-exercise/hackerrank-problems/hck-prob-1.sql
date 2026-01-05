cba39427d0dc3504abe36e428943eac1c5bd9b0d
Priyadharshni.G <155886762+PriyadharshniG@users.noreply.github.com>
2026-01-05 19:17:40 +0530

    Add new SQL queries for city and station data

--- sql-exercise/hackerrank-problems/hck-prob-1.sql
+++ sql-exercise/hackerrank-problems/hck-prob-1.sql
@@ -5,5 +5,18 @@ SELECT Name from City WHERE Population>120000 AND CountryCode='USA';
 
 SELECT * FROM City where ID=1661;
 
-
 SELECT Name from City where CountryCode='JPN';
+
+SELECT DISTINCT City from Station where MOD(ID,2)=0;
+
+SELECT COUNT(CITY)-COUNT(DISTINCT CITY) FROM Station;
+
+SELECT CITY, LENGTH(CITY)
+FROM STATION
+ORDER BY LENGTH(CITY), CITY --aggregate function like count cannnot be used in where clause directly
+LIMIT 1;
+
+SELECT CITY, LENGTH(CITY)
+FROM STATION
+ORDER BY LENGTH(CITY) DESC, CITY
+LIMIT 1;
